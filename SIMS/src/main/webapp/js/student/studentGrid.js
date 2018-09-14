35dad31a9d53c84d408684347a583076fa9f8d23
lwl1216191815 <long20150509@163.com>
2018-09-14 09:22:59 +0800

    实现了数据回显，修改功能未完成

--- SIMS/src/main/webapp/js/student/studentGrid.js
+++ SIMS/src/main/webapp/js/student/studentGrid.js
@@ -12,7 +12,10 @@ Ext.define('SIMS.StudentGrid', {
     initComponent: function () {
         var me = this;
         this.addEvents({
-            'addStudent':true
+            'addStudent' : true,
+            'winShow' : true,
+            'setWinTitle' : true,
+            'detailInfo' : true
         });
         me.columns = [
             {
@@ -23,22 +26,29 @@ Ext.define('SIMS.StudentGrid', {
             {
                 text: 'id',
                 dataIndex: 'id',
-                width: 150
+                width: 255
             },
             {
                 text: '学生姓名',
                 dataIndex: 'name',
-                width: 150
+                width: 80
             },
             {
                 text: '学生性别',
                 dataIndex: 'sex',
-                width: 150
+                width: 80,
+                renderer : function (value) {
+                    if(value == '0'){
+                        return '男';
+                    }else{
+                        return '女';
+                    }
+                }
             },
             {
                 text: '备注',
                 dataIndex: 'remark',
-                width: 150
+                width: 250
             }
         ];
         me.store = Ext.create('Ext.data.Store', {
@@ -61,9 +71,17 @@ Ext.define('SIMS.StudentGrid', {
                 {
                     text: '增加',
                     xType: 'button',
-                    iconCls: 'icon-custom-add',
+                    iconCls: 'icon-plus-sign-alt',
                     handler: function () {
                         me.addStudent();
+                    }
+                },
+                {
+                    text : '更改',
+                    xtype : 'button',
+                    iconCls : 'icon-edit',
+                    handler : function(){
+                        me.modifyStudent();
                     }
                 }
             ]
@@ -71,12 +89,50 @@ Ext.define('SIMS.StudentGrid', {
         me.callParent();
     },
     /**
-     * 添加学生的处理方法 ，触发事件
+     * 添加学生的处理方法
+     * 触发显示窗口事件
+     * 触发更换窗口标题事件
      */
    addStudent : function () {
        var me = this;
-       me.fireEvent('addStudent',me);
-   }
-
-
+       me.fireEvent('winShow',me);
+       me.fireEvent('setWinTitle','添加学生信息',me);
+   },
+    /**
+     * 获取表单的值，并且更新store
+     */
+    queryData : function () {
+        var me = this;
+        me.store.loadPage(1);
+    },
+    /**
+     * 更改学生信息的方法
+     */
+    modifyStudent : function () {
+        var me = this;
+        var selectData = me.getSelectedIds();
+        if (selectData.length > 1) {
+            Ext.Msg.alert("提示", "请选择一条记录操作", function (rep) {
+                if (rep == 'ok') {
+                    return;
+                }
+            });
+        }else{
+            var data = me.getSelectionModel().getLastSelected();
+            me.fireEvent('detailInfo',data,me);
+            me.fireEvent('winShow',me);
+            me.fireEvent('setWinTitle','修改学生信息',me);
+        }
+    },
+    /**
+     * 获取当前选中对象的id
+     */
+    getSelectedIds : function(){
+        var me = this;
+        var rows = me.getSelectionModel().getSelection();
+        if(rows.length == 0){
+              Ext.Msg.alert("警告","请选择一条记录");
+        }
+        return rows;
+    }
 });
