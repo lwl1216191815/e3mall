b09d1d3b9a259d7373563796c8a86364d9a381c9
lwl1216191815 <long20150509@163.com>
2020-04-18 11:15:10 +0800

    删除

--- SIMS/src/main/webapp/js/student/studentModel.js
+++
@@ -1,9 +0,0 @@
-Ext.define('SIMS.StudentModel',{
-    extend : 'Ext.data.Model',
-    fields:[
-        {name:'id'},
-        {name:'name'},
-        {name:'remark'},
-        {name:'sex'}
-    ]
-});
