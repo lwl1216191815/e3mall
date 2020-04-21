707cecd143224ab8d7bd92b4c928baa80bab06f9
lwl1216191815 <long20150509@163.com>
2020-04-21 13:37:10 +0800

    商品全文检索常量类

---
+++ e3-common/src/main/java/cn/e3mall/common/constant/SearchItemConstant.java
@@ -0,0 +1,29 @@
+package cn.e3mall.common.constant;
+
+public class SearchItemConstant {
+	
+	/**
+	 * 商品ID域
+	 */
+	public final static String ID = "id";
+	/**
+	 * 商品标题域
+	 */
+	public final static String ITEM_TITLE = "item_title";
+	/**
+	 * 商品卖点域
+	 */
+	public final static String ITEM_SELL_POINT = "item_sell_point";
+	/**
+	 * 商品价格域
+	 */
+	public final static String ITEM_PRICE = "item_price";
+	/**
+	 * 商品图片域
+	 */
+	public final static String ITEM_IMAGE = "item_image";
+	/**
+	 * 商品类型名称
+	 */
+	public final static String ITEM_CATEGORY_NAME = "item_category_name"; 
+}
