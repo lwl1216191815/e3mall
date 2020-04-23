a9f46c1ccbf4c9c69ab3907997370097574098a2
lwl1216191815 <long20150509@163.com>
2020-04-23 14:08:24 +0800

    添加redis缓存

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemDescServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemDescServiceImpl.java
@@ -2,7 +2,11 @@ package cn.e3mall.service.impl;
 
 import java.util.List;
 
+import cn.e3mall.common.jedis.JedisClient;
+import cn.e3mall.common.util.JsonUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import cn.e3mall.common.exception.ItemDescNotFoundException;
@@ -21,14 +25,28 @@ import cn.e3mall.service.ItemDescService;
 public class ItemDescServiceImpl implements ItemDescService {
 	@Autowired
 	private TbItemDescMapper itemDescMapper;
+    @Value("${redis.item.pre}")
+    private String redisItemPre;
+    @Value("${item.cache.expire}")
+    private Integer redisItemExpire;
+    @Autowired
+    private JedisClient jedisClient;
 	@Override
 	public E3Result getDescByItemId(long itemId) throws ItemDescNotFoundException {
-		TbItemDescExample example = new TbItemDescExample();
+        String json = jedisClient.get(redisItemPre + ":" + itemId + ":DESC");
+        if(StringUtils.isNotBlank(json)){
+            TbItemDesc itemDesc = JsonUtils.jsonToObject(json, TbItemDesc.class);
+            return E3Result.ok(itemDesc);
+        }
+        TbItemDescExample example = new TbItemDescExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andItemIdEqualTo(itemId);
 		List<TbItemDesc> list = itemDescMapper.selectByExampleWithBLOBs(example);
 		if(!list.isEmpty() && list.size() == 1 && list.get(0).getItemId() == itemId) {
-			return E3Result.ok(list.get(0));
+            TbItemDesc itemDesc = list.get(0);
+            jedisClient.set(redisItemPre + ":" + itemId + ":DESC",JsonUtils.objectToJson(itemDesc));
+            jedisClient.expire(redisItemPre + ":" + itemId + ":DESC",redisItemExpire);
+            return E3Result.ok(itemDesc);
 		}
 		throw new ItemDescNotFoundException("根据商品ID未找到合法的商品详情记录");
 	}
