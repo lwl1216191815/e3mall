4b243aa78bf9ccb3fd9db0a39e62bd640536b341
lwl1216191815 <long20150509@163.com>
2020-04-26 11:21:47 +0800

    完成用户注册

--- e3-sso/e3-sso-interface/src/main/java/cn/e3mall/sso/service/RegisterService.java
+++ e3-sso/e3-sso-interface/src/main/java/cn/e3mall/sso/service/RegisterService.java
@@ -1,6 +1,7 @@
 package cn.e3mall.sso.service;
 
 import cn.e3mall.common.util.E3Result;
+import cn.e3mall.pojo.TbUser;
 
 public interface RegisterService {
 
@@ -11,4 +12,11 @@ public interface RegisterService {
      * @return
      */
     E3Result checkData(String param,int type);
+
+    /**
+     * 用户注册
+     * @param tbUser
+     * @return
+     */
+    E3Result createUser(TbUser tbUser);
 }
