a7c82d063e1f3af10e26886775b0a410fe8a0ac5
lwl1216191815 <long20150509@163.com>
2020-04-26 14:54:01 +0800

    跳转到登录页面

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/PageController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/PageController.java
@@ -10,4 +10,9 @@ public class PageController {
     public String showRegister(){
         return "register";
     }
+
+    @RequestMapping(value = {"/page/login","/user/login"})
+    public String showLogin(){
+        return "login";
+    }
 }
