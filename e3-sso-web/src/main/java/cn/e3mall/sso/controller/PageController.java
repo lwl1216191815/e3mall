c6173115569a7acb96980f599c7d9d939a477a44
lwl1216191815 <long20150509@163.com>
2020-04-26 10:27:24 +0800

    页面跳转（以后一定要专门写个前端工程）

---
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/PageController.java
@@ -0,0 +1,13 @@
+package cn.e3mall.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/page/register")
+    public String showRegister(){
+        return "register";
+    }
+}
