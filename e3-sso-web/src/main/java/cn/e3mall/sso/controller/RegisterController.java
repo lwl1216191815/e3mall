a735fccda6a21d3bcb3b10fca4a3dfc17c1dbd1c
lwl1216191815 <long20150509@163.com>
2020-04-26 11:23:15 +0800

    实现用户注册

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
@@ -1,10 +1,12 @@
 package cn.e3mall.sso.controller;
 
 import cn.e3mall.common.util.E3Result;
+import cn.e3mall.pojo.TbUser;
 import cn.e3mall.sso.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +18,13 @@ public class RegisterController {
     @RequestMapping("/user/check/{param}/{type}")
     public E3Result checkData(@PathVariable String param, @PathVariable Integer type){
         E3Result result = registerService.checkData(param, type);
+        return result;
+    }
+
+
+    @RequestMapping(value="/user/register",method = RequestMethod.POST)
+    public E3Result register(TbUser tbUser){
+        E3Result result = registerService.createUser(tbUser);
         return result;
     }
 }
