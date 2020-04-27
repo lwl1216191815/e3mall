32c352dcd5cbb8736e75d7a2aae4ee7af5c1f34d
lwl1216191815 <long20150509@163.com>
2020-04-27 12:56:30 +0800

    查询购物车列表

--- e3-cart-web/src/main/java/cn/e3mall/cart/controller/CartController.java
+++ e3-cart-web/src/main/java/cn/e3mall/cart/controller/CartController.java
@@ -2,13 +2,16 @@ package cn.e3mall.cart.controller;
 
 
 import cn.e3mall.cart.service.CartService;
+import cn.e3mall.pojo.TbItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 购物车控制器
@@ -22,5 +25,13 @@ public class CartController {
     public String addCartItem(@PathVariable Long itemId, Integer num, HttpServletRequest request, HttpServletResponse response){
         cartService.addCartItem(itemId,num,request,response);
         return "cartSuccess";
+    }
+
+
+    @RequestMapping("/cart/cart")
+    public String showCartList(HttpServletRequest request, Model model){
+        List<TbItem> cartList = cartService.getCartList(request);
+        model.addAttribute("cartList",cartList);
+        return "cart";
     }
 }
