8d3ccad5362ab143c3de0f354da6c7c702469011
lwl1216191815 <long20150509@163.com>
2020-05-02 11:47:11 +0800

    服务端删除和添加购物车

--- e3-cart/e3-cart-interface/src/main/java/cn/e3mall/cart/service/CartService.java
+++ e3-cart/e3-cart-interface/src/main/java/cn/e3mall/cart/service/CartService.java
@@ -1,6 +1,10 @@
 package cn.e3mall.cart.service;
 
 import cn.e3mall.common.util.E3Result;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbUser;
+
+import java.util.List;
 
 /**
  * 购物车接口
@@ -14,4 +18,36 @@ public interface CartService {
      * @return
      */
     E3Result addCart(Long userId,Long itemId,Integer num);
+
+    /**
+     *  将cookie中的购物车合并到redis中
+     * @param userId
+     * @param itemList
+     * @return
+     */
+    E3Result mergeCart(Long userId, List<TbItem> itemList);
+
+    /**
+     * 根据用户ID获取购物车列表
+     * @param userId
+     * @return
+     */
+    List<TbItem> getCartList(Long userId);
+
+    /**
+     * 更新购物车数量
+     * @param userId
+     * @param itemId
+     * @param num
+     * @return
+     */
+    E3Result updateCartNum(Long userId,Long itemId,Integer num);
+
+    /**
+     * 删除购物车
+     * @param userId
+     * @param itemId
+     * @return
+     */
+    E3Result deleteCart(Long userId,Long itemId);
 }
